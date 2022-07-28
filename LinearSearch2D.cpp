ca1d92fe32a984b2f2325639ff98f0e477fdc8d4
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-07-28 22:36:43 +0500

    LinearSearch implemented on 2d arrays/vector

--- LinearSearch2D.cpp
+++ LinearSearch2D.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
-#include <vector> 
+#include <vector>
 using namespace std;
 
-
+vector<int> LinearSearch2D(vector<vector<int> > (arr), int target)
+{
+    vector<int> out = {-1, -1};
+    for (int row = 0; row < arr.size(); row++)
+    {
+        for (int col = 0; col < arr[row].size(); col++)
+        {
+            if (arr[row][col] == target)
+            {
+                out[0] = row;
+                out[1] = col;
+            }
+        }
+    }
+    return out;
+}
 
 int main(int argc, char const *argv[])
 {
-
-    
+    vector<vector<int> > in{
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    vector<int> ans = LinearSearch2D((in), 6);
+    cout << ans[0] << " " << ans[1] << endl;
     return 0;
 }
